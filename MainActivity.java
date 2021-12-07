7c863034659c55a0fcfbfd05b908f9b7422e6398
박세현 <94225183+sehyunPark-github@users.noreply.github.com>
2021-12-07 20:51:23 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -28,36 +28,36 @@ public class MainActivity extends Activity {
     Calendar calendar;
     int currentYear, currentMonth, currentDay;
 
-    // Millisecond 형태의 하루(24 시간)
-    private final int ONE_DAY = 24 * 60 * 60 * 1000;
+    // 밀리세컨드형태의 하루(24 시간), 디데이계산하려면 밀리세컨드여야함
+    private final int ONE_DAY = 24 * 60 * 60 * 1000; //변경불가
 
     TextView edit_endDateBtn, edit_result;
-    Button datePicker;
+    Button datePicker; //날짜선택버튼
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @Override //오버라이드
+    public void onCreate(Bundle savedInstanceState) { //액티비티간에 데이터 주고받을 때 번들클래스 사용하여 데이터 전송
+        super.onCreate(savedInstanceState); //상위클래스의 생성자 호출, 이전상태 포함 번들
+        setContentView(R.layout.activity_main); //사용자가 볼 레이아웃 메서드도 호출
 
         //시작일, 종료일 데이터 저장
-        calendar = Calendar.getInstance();
-        currentYear = calendar.get(Calendar.YEAR);
-        currentMonth = (calendar.get(Calendar.MONTH));
-        currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+        calendar = Calendar.getInstance(); //캘린더인스턴스 생성
+        currentYear = calendar.get(Calendar.YEAR); //현재년도
+        currentMonth = (calendar.get(Calendar.MONTH)); //현재월
+        currentDay = calendar.get(Calendar.DAY_OF_MONTH); //현재일
 
         datePicker = (Button) findViewById(R.id.datePicker);
-        edit_endDateBtn = (TextView) findViewById(R.id.edit_endDateBtn);
-        edit_result = (TextView) findViewById(R.id.edit_result);
+        edit_endDateBtn = (TextView) findViewById(R.id.edit_endDateBtn); //입력날짜표시하는곳
+        edit_result = (TextView) findViewById(R.id.edit_result); //디데이결과표시하는 곳
 
         //한국어 설정 (ex: date picker)
         Locale.setDefault(Locale.KOREAN);
 
-        // 디데이 날짜 입력
+        // 디데이 날짜 입력(처음세팅 오늘날짜)
         edit_endDateBtn.setText(currentYear + "년 " + (currentMonth + 1) + "월 " + currentDay + "일");
 
         //datePicker : 디데이 날짜 입력하는 버튼, 클릭시 DatePickerDialog 띄우기
         datePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
+            @Override //이 메서드가 기반형식에 정의한 메서드를 덮어씀,이거만 실행
             public void onClick(View view) {
                 new DatePickerDialog(MainActivity.this, endDateSetListener, (currentYear), (currentMonth), currentDay).show();
             }
@@ -67,7 +67,7 @@ public class MainActivity extends Activity {
 
 
     /** @brief endDateSetListener
-     *  @date 2016-02-18
+     *  @date 2021-12-5
      *  @detail DatePickerDialog띄우기, 종료일 저장, 기존에 입력한 값이 있으면 해당 데이터 설정후 띄우기
      */
     private DatePickerDialog.OnDateSetListener endDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -94,7 +94,7 @@ public class MainActivity extends Activity {
         ddayCalendar.set(mYear, mMonthOfYear, mDayOfMonth);
 
         // D-day 를 구하기 위해 millisecond 으로 환산하여 d-day 에서 today 의 차를 구한다.
-        final long dday = ddayCalendar.getTimeInMillis() / ONE_DAY;
+        final long dday = ddayCalendar.getTimeInMillis() / ONE_DAY; //(하루)의 밀리초수
         final long today = Calendar.getInstance().getTimeInMillis() / ONE_DAY;
         long result = dday - today;
 
@@ -103,7 +103,7 @@ public class MainActivity extends Activity {
         if (result > 0) {
             strFormat = "D-%d";
         } else if (result == 0) {
-            strFormat = "Today";
+            strFormat = "오늘입니다";
         } else {
             result *= -1;
             strFormat = "D+%d";
